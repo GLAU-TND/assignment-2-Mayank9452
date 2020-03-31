968d5dade61c4a5266e69c8fb965c33e8cca6c36
Mayank Kumar Singh <mayank.singh_cs18@gla.ac.in>
2020-03-31 18:12:53 +0530

    getter and setter method for front and rear field

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,5 +7,26 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.Node.Node;
+
 public class MyQueue {
+    private Node front;
+    private Node rear;
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getRear() {
+        return rear;
+    }
+
+    public void setRear(Node rear) {
+        this.rear = rear;
+    }
 }
+
